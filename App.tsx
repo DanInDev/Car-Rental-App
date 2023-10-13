c7b65927b09f419163f6abfb80d8cc662a3c4ca1
OFHansen <ohans21@student.sdu.dk>
2023-10-13 15:00:22 +0200

    homepage

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontWeight: 'bold', fontSize: 25}}>Welcome to</Text>
+      <Text style={{fontWeight: 'bold', fontSize: 40}}>Peter's Car Rental</Text>
+
+      <Button title="Login" onPress={() => console.log("Login")} />
+
+      <Button title="Home" onPress={() => console.log("Home")} />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#808080',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
